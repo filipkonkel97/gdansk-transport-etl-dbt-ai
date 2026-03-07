d549339d04ed756608ebdd434e77d516c2eba9c9
filipkonkel97 <filipkonkel97@gmail.com>
2026-03-07 11:49:52 +0100

    feat(airflow): add dbt task groups to all DAGs and update cron schedules

--- dags/buses_delay_pipeline.py
+++ dags/buses_delay_pipeline.py
@@ -1,14 +1,16 @@
 from airflow.sdk import dag, task
 from datetime import datetime, timedelta
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig, LoadMode
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 import os
 import pandas as pd
 
 @dag(
     dag_id="gdansk_public_transport_delays_pipeline_w_dbt",
     description='Python + Snowflake + dbt pipeline',
-    start_date=datetime.utcnow() - timedelta(days=1),
-    schedule='@daily',
+    start_date=datetime(2024, 1, 1),
+    schedule='1/3 * * * *',
     catchup=False,
     tags=['python', 'snowflake','dbt']
 )
@@ -49,10 +51,37 @@ def delays_pipeline():
 
         loader.load_data(df, schema='buses.bronze', table_name='bronze_delays')
 
+    profile_config = ProfileConfig(
+        profile_name = "snowflake_profile",
+        target_name = "dev",
+        profile_mapping = SnowflakeUserPasswordProfileMapping(
+            conn_id = "snowflake_conn",
+            profile_args = {
+                "schema": "buses",
+                "database": "buses"
+            }
+        )
+    )
+    
+    delays_dbt_group = DbtTaskGroup(
+        project_config = ProjectConfig(
+            dbt_project_path = "/usr/local/airflow/dags/dbt"
+        ),
+        profile_config = profile_config,
+        execution_config = ExecutionConfig(
+            dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt"
+        ),
+        group_id = "dbt_delays_run",
+        render_config = RenderConfig(
+            selector="buses_delays_only",
+            load_method=LoadMode.DBT_LS,
+        )
+    )
+
     extracted = extract_buses_delays_data()
     transformed = transform_buses_delays_data(extracted)
     loaded = load_buses_delays_data(transformed)
 
-    extracted >> transformed >> loaded
+    extracted >> transformed >> loaded >> delays_dbt_group
 
 delays_pipeline()
--- dags/vehicles_database_pipeline.py
+++ dags/vehicles_database_pipeline.py
@@ -1,14 +1,16 @@
 from airflow.sdk import dag, task
 from datetime import datetime, timedelta
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig, LoadMode
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 import os
 import pandas as pd
 
 @dag(
     dag_id="gdansk_public_transport_vehicles_w_dbt_pipeline",
     description='Python + Snowflake + dbt pipeline',
-    start_date=datetime.utcnow() - timedelta(days=1),
-    schedule='@daily',
+    start_date=datetime(2024, 1, 1),
+    schedule='0 20 * * 1',
     catchup=False,
     tags=['python', 'snowflake','dbt']
 )
@@ -49,10 +51,37 @@ def vehicles_pipeline():
 
         loader.load_data(df, schema='buses.bronze', table_name='bronze_vehicles')
 
+    profile_config = ProfileConfig(
+        profile_name = "snowflake_profile",
+        target_name = "dev",
+        profile_mapping = SnowflakeUserPasswordProfileMapping(
+            conn_id = "snowflake_conn",
+            profile_args = {
+                "schema": "buses",
+                "database": "buses"
+            }
+        )
+    )
+    
+    vehicles_dbt_group = DbtTaskGroup(
+        project_config = ProjectConfig(
+            dbt_project_path = "/usr/local/airflow/dags/dbt"
+        ),
+        profile_config = profile_config,
+        execution_config = ExecutionConfig(
+            dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt"
+        ),
+        group_id = "dbt_vehicles_run",
+        render_config = RenderConfig(
+            selector="vehicles_only",
+            load_method=LoadMode.DBT_LS,
+        )
+    )
+
     extracted = extract_vehiclesdb_data()
     transformed = transform_vehiclesdb_data(extracted)
     loaded = load_vehiclesdb_data(transformed)
 
-    extracted >> transformed >> loaded
+    extracted >> transformed >> loaded >> vehicles_dbt_group
 
 vehicles_pipeline()
